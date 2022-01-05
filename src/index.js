6191ede8d5c4f9420c7d91e5b85f6ba93c8a2a85
Geetansh Atrey <atrey.geetansh@gmail.com>
2022-01-05 18:26:37 +0530

    Comment Section Component
    This is a simple React project which shows a list of comments in a blog. The UI consists of a comment box, inside which at the very top - the avatar, username, timestamp, & their message is present. At the bottom of the box, a couple of buttons are present for Approve & Reject.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className='ui container comments'>
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Sam" 
+                    avatarId="3" 
+                    timeAgo="Today at 8:15 PM" 
+                    text="Awesome!" 
+                />
+            </ApprovalCard>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+            <ApprovalCard>
+                <CommentDetail  
+                    author="Kevin" 
+                    avatarId="4" 
+                    timeAgo="Today at 7:54 PM" 
+                    text="Really liked it :)" 
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author="Jennifer" 
+                    avatarId="5" 
+                    timeAgo="Yesterday at 8:39 PM" 
+                    text="Good read."
+                />
+            </ApprovalCard>  
+        </div>
+    );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
